114bbdaeaffa7dd8a6e84a0a54069673b659bf41
Adham Zahran <lordadamson@gmail.com>
2017-08-23 16:42:05 +0200

    using vectors instead of arrays and std::wstring(S_LETTER)

--- ar_letter_definitions.h
+++ ar_letter_definitions.h
@@ -2,6 +2,7 @@
 #define AR_LETTER_DEFINITIONS_H
 
 #include <string>
+#include <vector>
 
 #define HAMZA            L'\u0621'
 #define ALEF_MADDA       L'\u0622'
@@ -50,53 +51,53 @@
 #define SKOON            L'\u0652'
 
 // same thing but strings instead of chars
-#define S_HAMZA            L"\u0621"
-#define S_ALEF_MADDA       L"\u0622"
-#define S_ALEF_HAMZA_ABOVE L"\u0623"
-#define S_WAW_HAMZA_ABOVE  L"\u0624"
-#define S_ALEF_HAMZA_BELOW L"\u0625"
-#define S_YEH_HAMZA_ABOVE  L"\u0626"
-#define S_ALEF_NO_HAMZA    L"\u0627"
-#define S_BEH              L"\u0628"
-#define S_TEH_MARBOOTA     L"\u0629"
-#define S_TEH              L"\u062A"
-#define S_THEH             L"\u062B"
-#define S_JEEM             L"\u062C"
-#define S_HAH              L"\u062D"
-#define S_KHAH             L"\u062E"
-#define S_DAL              L"\u062F"
-#define S_THAL             L"\u0630"
-#define S_REH              L"\u0631"
-#define S_ZAIN             L"\u0632"
-#define S_SEEN             L"\u0633"
-#define S_SHEEN            L"\u0634"
-#define S_SAD              L"\u0635"
-#define S_DAD              L"\u0636"
-#define S_TAH              L"\u0637"
-#define S_ZAH              L"\u0638"
-#define S_AIN              L"\u0639"
-#define S_GHAIN            L"\u063A"
-#define S_FEH              L"\u0641"
-#define S_QAF              L"\u0642"
-#define S_KAF              L"\u0643"
-#define S_LAM              L"\u0644"
-#define S_MEEM             L"\u0645"
-#define S_NOON             L"\u0646"
-#define S_HEH              L"\u0647"
-#define S_WAW              L"\u0648"
-#define S_YEH_NO_DOTS      L"\u0649"
-#define S_YEH_WITH_DOTS    L"\u064A"
-#define S_TATWEEL          L"\u0640"
-#define S_TANWEEN_FATHA    L"\u064b"
-#define S_TANWEEN_DAMA     L"\u064c"
-#define S_TANWEEN_KASRA    L"\u064d"
-#define S_FATHA            L"\u064e"
-#define S_DAMA             L"\u064f"
-#define S_KASRA            L"\u0650"
-#define S_SHADA            L"\u0651"
-#define S_SKOON            L"\u0652"
+#define S_HAMZA            std::wstring(L"\u0621")
+#define S_ALEF_MADDA       std::wstring(L"\u0622")
+#define S_ALEF_HAMZA_ABOVE std::wstring(L"\u0623")
+#define S_WAW_HAMZA_ABOVE  std::wstring(L"\u0624")
+#define S_ALEF_HAMZA_BELOW std::wstring(L"\u0625")
+#define S_YEH_HAMZA_ABOVE  std::wstring(L"\u0626")
+#define S_ALEF_NO_HAMZA    std::wstring(L"\u0627")
+#define S_BEH              std::wstring(L"\u0628")
+#define S_TEH_MARBOOTA     std::wstring(L"\u0629")
+#define S_TEH              std::wstring(L"\u062A")
+#define S_THEH             std::wstring(L"\u062B")
+#define S_JEEM             std::wstring(L"\u062C")
+#define S_HAH              std::wstring(L"\u062D")
+#define S_KHAH             std::wstring(L"\u062E")
+#define S_DAL              std::wstring(L"\u062F")
+#define S_THAL             std::wstring(L"\u0630")
+#define S_REH              std::wstring(L"\u0631")
+#define S_ZAIN             std::wstring(L"\u0632")
+#define S_SEEN             std::wstring(L"\u0633")
+#define S_SHEEN            std::wstring(L"\u0634")
+#define S_SAD              std::wstring(L"\u0635")
+#define S_DAD              std::wstring(L"\u0636")
+#define S_TAH              std::wstring(L"\u0637")
+#define S_ZAH              std::wstring(L"\u0638")
+#define S_AIN              std::wstring(L"\u0639")
+#define S_GHAIN            std::wstring(L"\u063A")
+#define S_FEH              std::wstring(L"\u0641")
+#define S_QAF              std::wstring(L"\u0642")
+#define S_KAF              std::wstring(L"\u0643")
+#define S_LAM              std::wstring(L"\u0644")
+#define S_MEEM             std::wstring(L"\u0645")
+#define S_NOON             std::wstring(L"\u0646")
+#define S_HEH              std::wstring(L"\u0647")
+#define S_WAW              std::wstring(L"\u0648")
+#define S_YEH_NO_DOTS      std::wstring(L"\u0649")
+#define S_YEH_WITH_DOTS    std::wstring(L"\u064A")
+#define S_TATWEEL          std::wstring(L"\u0640")
+#define S_TANWEEN_FATHA    std::wstring(L"\u064b")
+#define S_TANWEEN_DAMA     std::wstring(L"\u064c")
+#define S_TANWEEN_KASRA    std::wstring(L"\u064d")
+#define S_FATHA            std::wstring(L"\u064e")
+#define S_DAMA             std::wstring(L"\u064f")
+#define S_KASRA            std::wstring(L"\u0650")
+#define S_SHADA            std::wstring(L"\u0651")
+#define S_SKOON            std::wstring(L"\u0652")
 
-extern const wchar_t arabic_letters_with_tashkeel[] =
+extern const std::vector<wchar_t> arabic_letters_with_tashkeel =
 {
 	HAMZA           ,
 	ALEF_MADDA      ,
@@ -145,7 +146,7 @@ extern const wchar_t arabic_letters_with_tashkeel[] =
 	SKOON
 };
 
-extern const wchar_t arabic_letters_without_tashkeel[] =
+extern const std::vector<wchar_t> arabic_letters_without_tashkeel =
 {
 	HAMZA           ,
 	ALEF_MADDA      ,
@@ -185,7 +186,7 @@ extern const wchar_t arabic_letters_without_tashkeel[] =
 	YEH_WITH_DOTS
 };
 
-extern const std::wstring s_arabic_letters_with_tashkeel[] =
+extern const std::vector<std::wstring> s_arabic_letters_with_tashkeel =
 {
 	S_HAMZA           ,
 	S_ALEF_MADDA      ,
@@ -234,7 +235,7 @@ extern const std::wstring s_arabic_letters_with_tashkeel[] =
 	S_SKOON
 };
 
-extern const std::wstring s_arabic_letters_without_tashkeel[] =
+extern const std::vector<std::wstring> s_arabic_letters_without_tashkeel =
 {
 	S_HAMZA           ,
 	S_ALEF_MADDA      ,
